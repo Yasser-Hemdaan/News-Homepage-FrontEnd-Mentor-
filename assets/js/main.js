2e7bf0f6dcb1e84011632680141fef87c12e9595
Yasser Hemdan <YasserHemdaan@gmail.com>
2023-04-27 16:00:51 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var menuList = document.querySelector(".menuList");
-  var burger = document.querySelector(".burger");
-  var close = document.querySelector(".close");
-  burger.addEventListener("click", () => {
-    menuList.style.right = "0";
+  let list = document.querySelectorAll(".list li a");
+  console.log(list);
+  list.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      list.forEach((delElement) => {
+        delElement.classList.remove("active");
+      });
+      e.target.classList.add("active");
+    });
   });
-  close.addEventListener("click", () => {
-    menuList.style.right = "";
-  });
+});
+
+let menuList = document.querySelector(".menuList");
+let menuIcon = document.querySelector(".menuIcon");
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
+  menuList.classList.toggle("active");
 });
